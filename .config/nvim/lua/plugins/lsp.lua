2841170ffa8047f9abb82f365b1e3ee0193a7081
Pascal Sochacki <pascal@sochacki.dev>
2024-02-26 22:45:15 +0100

    add obsidian

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -49,7 +49,7 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "helm-ls",
+                "helm_ls",
                 "yamlls",
                 "tsserver",
             },

36b6b8000ae7e51f22808a46eb996bccde68de6b
Pascal Sochacki <pascal@sochacki.dev>
2024-03-11 09:22:27 +0100

    split lsp and cmp

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,21 +1,22 @@
-
 return {
     'stevearc/conform.nvim',
     opts = {
-      notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        go = { 'gofmt' }
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
-      },
+        notify_on_error = true,
+        format_on_save = {
+            timeout_ms = 5000,
+            lsp_fallback = true,
+        },
+        formatters_by_ft = {
+            lua = { 'stylua' },
+            go = { 'gofmt' },
+            -- Conform can also run multiple formatters sequentially
+            -- python = { "isort", "black" },
+            --
+            -- You can use a sub-list to tell conform to run *until* a formatter
+            -- is found.
+            javascript = { "prettier" },
+            yaml = { "prettier" },
+            typescript = { "prettier" },
+        },
     },
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,17 +3,8 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
         "b0o/schemastore.nvim",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
         'towolf/vim-helm',
-        "neovim/nvim-lspconfig",
-        "rafamadriz/friendly-snippets",
         "j-hui/fidget.nvim",
     },
     config = function()
@@ -27,25 +18,7 @@ return {
 
         require("fidget").setup({})
         require("mason").setup()
-        local luasnip = require("luasnip")
-        luasnip.config.set_config({
-            history = true,
-            updateevents = "TextChanged,TextChangedI"
-        })
 
-        vim.keymap.set({"i", "s"}, "<c-k>", function ()
-            if luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            end
-        end, {silent = true})
-
-        vim.keymap.set({"i", "s"}, "<c-j>", function ()
-            if luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            end
-        end, {silent = true})
-
-        require("luasnip.loaders.from_vscode").lazy_load()
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
@@ -59,7 +32,7 @@ return {
                         capabilities = capabilities
                     }
                 end,
-                ["helm_ls"] = function ()
+                ["helm_ls"] = function()
                     local lspconfig = require('lspconfig')
                     lspconfig.helm_ls.setup {
                         settings = {
@@ -70,9 +43,8 @@ return {
                             }
                         }
                     }
-
                 end,
-                ["jsonls"] = function ()
+                ["jsonls"] = function()
                     local lspconfig = require("lspconfig")
                     lspconfig.jsonls.setup {
                         settings = {
@@ -102,11 +74,11 @@ return {
                                     url = ""
                                 },
                                 schemas = {
-                                    ["https://json.schemastore.org/kustomization.json"] = "kustomization.{yml,yaml}" ,
-                                    ["https://json.schemastore.org/helmfile.json"] = "helmfile.{yml,yaml}" ,
-                                    ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "/*docker-compose*.{yml,yaml}" ,
-                                    ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}" ,
-                                    kubernetes =  "/*.yaml"
+                                    ["https://json.schemastore.org/kustomization.json"] = "kustomization.{yml,yaml}",
+                                    ["https://json.schemastore.org/helmfile.json"] = "helmfile.{yml,yaml}",
+                                    ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "/*docker-compose*.{yml,yaml}",
+                                    ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
+                                    kubernetes = "/*.yaml"
                                 }
                             }
                         }
@@ -128,27 +100,6 @@ return {
                 end
             }
         })
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-                { name = 'buffer' },
-                { name = "path" },
-            })
-        })
-
         vim.diagnostic.config({
             -- update_in_insert = true,
             float = {

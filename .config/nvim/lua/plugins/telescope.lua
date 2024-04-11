ed7ac6f6de2a568fd9a108c9d66cfe987b50451b
Pascal Sochacki <pascal@sochacki.dev>
2024-04-11 21:51:46 +0200

    update

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,15 +2,12 @@ return {
 
     "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
-
-
     dependencies = {
         "nvim-lua/plenary.nvim"
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup()
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})

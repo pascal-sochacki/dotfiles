9e7c7039c4d21fa8554719eb038661e62d854bdf
Pascal Sochacki <pascal@sochacki.dev>
2024-08-13 09:56:05 +0200

    add treesitter textobject

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,47 +1,83 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all"
-            ensure_installed = {
-                "vimdoc", "javascript", "typescript", "c", "lua",
-                "jsdoc", "bash", "yaml", "tsx", "prisma", "go"
-            },
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                -- A list of parser names, or "all"
+                ensure_installed = {
+                    "vimdoc", "javascript", "typescript", "c", "lua",
+                    "jsdoc", "bash", "yaml", "tsx", "prisma", "go"
+                },
 
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            auto_install = true,
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
 
-            indent = {
-                enable = true,
-                disable = { 'yaml' }
-            },
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
+                auto_install = true,
 
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
+                indent = {
+                    enable = true,
+                    disable = { 'yaml' }
+                },
 
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = { "markdown" },
-            },
-        })
+                highlight = {
+                    -- `false` will disable the whole extension
+                    enable = true,
 
-        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        treesitter_parser_config.templ = {
-            install_info = {
-                url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                files = { "src/parser.c", "src/scanner.c" },
-                branch = "master",
-            },
-        }
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = { "markdown" },
+                },
+            })
 
-        vim.treesitter.language.register("templ", "templ")
-    end
+            local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+            treesitter_parser_config.templ = {
+                install_info = {
+                    url = "https://github.com/vrischmann/tree-sitter-templ.git",
+                    files = { "src/parser.c", "src/scanner.c" },
+                    branch = "master",
+                },
+            }
+
+            vim.treesitter.language.register("templ", "templ")
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+
+                        -- Automatically jump forward to textobj, similar to targets.vim
+                        lookahead = true,
+
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                            -- nvim_buf_set_keymap) which plugins like which-key display
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            -- You can also use captures from other query groups like `locals.scm`
+                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        include_surrounding_whitespace = true,
+                    },
+
+                }
+            })
+        end
+
+    }
+
+
 }

558c1cdfd5fd3979ec597e801f96596b3e624429
Pascal Sochacki <pascal@sochacki.dev>
2024-09-02 19:26:20 +0200

    copy from LazyVim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,41 +3,44 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function()
-            local configs = require("nvim-treesitter.configs")
+        event = { "VeryLazy" },
+        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+        opts_extend = { "ensure_installed" },
 
-            configs.setup({
+        opts = {
 
-                -- A list of parser names, or "all"
-                ensure_installed = {
-                    "vimdoc", "javascript", "typescript", "c", "lua",
-                    "jsdoc", "bash", "yaml", "tsx", "prisma", "go"
-                },
+            -- A list of parser names, or "all"
+            ensure_installed = {
+                "vimdoc", "javascript", "typescript", "c", "lua",
+                "jsdoc", "bash", "yaml", "tsx", "prisma", "go"
+            },
 
-                -- Install parsers synchronously (only applied to `ensure_installed`)
-                sync_install = false,
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
 
-                -- Automatically install missing parsers when entering buffer
-                -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-                auto_install = false,
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
+            auto_install = false,
 
-                indent = {
-                    enable = true,
-                    disable = { 'yaml' }
-                },
+            indent = {
+                enable = true,
+                disable = { 'yaml' }
+            },
 
-                highlight = {
-                    -- `false` will disable the whole extension
-                    enable = true,
+            highlight = {
+                -- `false` will disable the whole extension
+                enable = true,
 
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
-                    additional_vim_regex_highlighting = { "markdown" },
-                },
-            })
-        end
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+                -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                -- Instead of true it can also be a list of languages
+                additional_vim_regex_highlighting = { "markdown" },
+            }
+        },
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+        end,
     },
     {
         "nvim-treesitter/nvim-treesitter-textobjects",

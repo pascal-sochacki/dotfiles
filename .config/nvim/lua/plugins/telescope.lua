81d9faa0fd88b1c6321ed9a559fd73c058d3dd39
Pascal Sochacki <pascal@sochacki.dev>
2024-09-02 19:46:01 +0200

    display hidden files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,7 +7,50 @@ return {
     },
 
     config = function()
-        require('telescope').setup()
+        require('telescope').setup({
+
+
+            defaults = {
+                -- configure to use ripgrep
+                vimgrep_arguments = {
+
+                    "rg",
+                    "--follow", -- Follow symbolic links
+                    "--hidden", -- Search for hidden files
+
+                    -- Exclude some patterns from search
+                    "--glob=!**/.git/*",
+                    "--glob=!**/.idea/*",
+                    "--glob=!**/.vscode/*",
+                    "--glob=!**/build/*",
+                    "--glob=!**/dist/*",
+                    "--glob=!**/yarn.lock",
+                    "--glob=!**/package-lock.json",
+
+                },
+
+
+            },
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    -- needed to exclude some files & dirs from general search
+                    -- when not included or specified in .gitignore
+                    find_command = {
+                        "rg",
+                        "--files",
+                        "--hidden",
+                        "--glob=!**/.git/*",
+                        "--glob=!**/.idea/*",
+                        "--glob=!**/.vscode/*",
+                        "--glob=!**/build/*",
+                        "--glob=!**/dist/*",
+                        "--glob=!**/yarn.lock",
+                        "--glob=!**/package-lock.json",
+                    },
+                },
+            },
+        })
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})

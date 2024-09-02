2b30d34654cacfbbea5a90cc07ad48b42bce369a
Pascal Sochacki <pascal@sochacki.dev>
2024-09-02 19:48:32 +0200

    grep

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -15,8 +15,13 @@ return {
                 vimgrep_arguments = {
 
                     "rg",
-                    "--follow", -- Follow symbolic links
-                    "--hidden", -- Search for hidden files
+                    "--follow",        -- Follow symbolic links
+                    "--hidden",        -- Search for hidden files
+                    "--no-heading",    -- Don't group matches by each file
+                    "--with-filename", -- Print the file path with the matched lines
+                    "--line-number",   -- Show line numbers
+                    "--column",        -- Show column numbers
+                    "--smart-case",    -- Smart case search
 
                     -- Exclude some patterns from search
                     "--glob=!**/.git/*",
